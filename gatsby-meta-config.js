1762990747e59e6aa7d736a6966bcac0cff552c3
tmkimm <xoals1417@gmail.com>
2020-09-15 23:48:49 +0900

    카테고리 정리

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `tmkimm's devlog`,
+  title: `tmkimm log🦄`,
   description: `tmkimm의 개발 블로그입니다. 공부한 내용을 모아두고 있습니다.`,
   author: `tmkimm`,
   introduction: `🧙‍♀️ 편하게 적는 개발로그`,
-  siteUrl: `https://tmkimm.netlify.com`, // Your blog site url
+  siteUrl: `https://tmkimm-log.netlify.app/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `tmkimm`, // Your GitHub account
@@ -12,10 +12,10 @@ module.exports = {
     linkedin: ``, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`blog`, `backend`, `frontend`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'tmkimm/tmkimm-log', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
